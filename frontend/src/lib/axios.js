e2c92962267d2903fd1282be8ddfadb3858d14b2
naveen8076 <167600306+naveen8076@users.noreply.github.com>
2025-10-09 20:11:23 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://buzzchat-zgwz.onrender.com/",
+  baseURL: import.meta.env.MODE === https://buzzchat-zgwz.onrender.com/,
   withCredentials: true,
 });
